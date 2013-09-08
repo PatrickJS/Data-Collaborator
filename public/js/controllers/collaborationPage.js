0d9499f4b5b4cbd3f30f520f7ef9f820e77b20fb
Alex Gaputin <googamanga@yahoo.com>
2013-09-08 04:12:05 -0700

    added the other two graphs

--- public/js/controllers/collaborationPage.js
+++ public/js/controllers/collaborationPage.js
@@ -4,6 +4,7 @@ window.angular.module('pmd.controllers.collaborationPage', [])
 
         $scope.bubbleD3 = $http.get('data/BubbleData.json');
         $scope.caloriesD3 = $http.get('data/Calories.csv');
+        $scope.stepsD3 = $http.get('data/Steps.csv');
 
         var bubbleD3 = function (){
             var diameter = 490,
@@ -43,9 +44,9 @@ window.angular.module('pmd.controllers.collaborationPage', [])
         };
 
         var caloriesD3 = function (){
-            var margin = {top: 20, right: 20, bottom: 30, left: 40},
-                width = 960 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
+            var margin = {top: 20, right: 20, bottom: 50, left: 40},
+                width = 650 - margin.left - margin.right,
+                height = 400 - margin.top - margin.bottom;
 
             var formatPercent = d3.format();
 
@@ -106,6 +107,71 @@ window.angular.module('pmd.controllers.collaborationPage', [])
             }
         };
 
+
+        var stepsD3 = function (){
+            var margin = {top: 20, right: 20, bottom: 50, left: 40},
+                width = 650 - margin.left - margin.right,
+                height = 400 - margin.top - margin.bottom;
+
+            var formatPercent = d3.format();
+
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .tickFormat(formatPercent);
+
+            var svg = d3.select("#d3-steps-chart").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            d3.csv("/data/Steps.csv", type, function(error, data) {
+              x.domain(data.map(function(d) { return d.Hour; }));
+              y.domain([0, d3.max(data, function(d) { return d.Steps; })]);
+
+              svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(xAxis);
+
+              svg.append("g")
+                  .attr("class", "y axis")
+                  .call(yAxis)
+                .append("text")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", 6)
+                  .attr("dy", ".71em")
+                  .style("text-anchor", "end")
+                  .text("Frequency");
+
+              svg.selectAll(".bar")
+                  .data(data)
+                .enter().append("rect")
+                  .attr("class", "bar")
+                  .attr("x", function(d) { return x(d.Hour); })
+                  .attr("width", x.rangeBand())
+                  .attr("y", function(d) { return y(d.Steps); })
+                  .attr("height", function(d) { return height - y(d.Steps); });
+
+            });
+
+            function type(d) {
+              d.Steps = +d.Steps;
+              return d;
+            }
+        };
+
         var turnOnAnnotator = function(){
             $('body').annotator();
         }
@@ -127,5 +193,6 @@ window.angular.module('pmd.controllers.collaborationPage', [])
         turnOnAnnotator();
         bubbleD3();
         caloriesD3();
+        stepsD3();
     }
  ]);
