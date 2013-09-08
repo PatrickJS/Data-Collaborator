fdaaa886e47529201bbf2f825b4f41b3fb03e3d4
C. James Winslow <whoaa512@gmail.com>
2013-09-07 19:02:54 -0700

    Renamed visual_picker to visualsList; added some styling

--- public/js/config.js
+++ public/js/config.js
@@ -5,8 +5,8 @@ window.app.config(['$routeProvider', function($routeProvider) {
   .when('/', {
     templateUrl: '/views/index.html'
   })
-  .when('/visualpicker', {
-    templateUrl: '/views/visual_picker.html'
+  .when('/visualslist', {
+    templateUrl: '/views/visuals_list.html'
   })
   .when('/vizpick',{
     templateUrl: '/views/vizpick.html',
